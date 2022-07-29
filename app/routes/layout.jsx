43168eb6ad149dc4506b5f35a1b7ddf0718307e0
Raul Soledispa <rsoledispa@technisys.com>
2022-07-29 09:05:57 -0500

    Layout wrapper modified

--- app/routes/layout.jsx
+++ app/routes/layout.jsx
@@ -2,10 +2,11 @@
 
 const Layout = ({children}) => {
   return (
-    /*<main className="full relative overflow-x-hidden expand bg-white">*/
-    <main className="max-w-[80rem] w-full mx-auto bg-transparent">
-      {children}
-    </main>
+    <>
+      <main className="max-w-[80rem] w-full mx-auto bg-transparent">
+        {children}
+      </main>
+    </>
   )
 }
 
