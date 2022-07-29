e5f3683f0f72956e6cd9ed6b6730fd3b81769ec7
Raul Soledispa <rsoledispa@technisys.com>
2022-07-29 09:43:09 -0500

    Custom navigation link added in header

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -10,6 +10,7 @@ import { createUserSession, getUserId } from "~/session.server";
 import { verifyLogin } from "~/models/user.server";
 import { safeRedirect } from "~/utils";
 import FieldInput from "../components/fieldInput";
+import Layout from "./layout";
 
 const loginSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
@@ -27,11 +28,9 @@ export async function action({ request }) {
   const formValues = Object.fromEntries(formData)
   const email = formData.get("email");
   const password = formData.get("password");
-  const redirectTo = safeRedirect(formData.get("redirectTo"), "/home");
+  const redirectTo = safeRedirect(formData.get("redirectTo"), "home");
   const result = loginSchema.safeParse(formValues);
 
-  console.log("result ", result)
-  console.log("result ", result.error)
   if(!result.success) return json({errors: result.error.issues})
 
   const user = await verifyLogin(email, password);
@@ -45,7 +44,7 @@ export async function action({ request }) {
 
   return createUserSession({
     request,
-    userId: user.userID,
+    userId: user.id,
     remember: true,//remember: remember === "on" ? true : false,
     redirectTo,
   });
@@ -66,36 +65,38 @@ export default function LoginPage() {
   const isSubmitting = Boolean(transition.submission)
 
   return (
-    <div className="min-h-screen bg-white flex flex-col justify-center py-12 px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in</h2>
-      </div>
+    <Layout>
+      <div className="flex flex-col justify-center items-center py-12 px-6 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in</h2>
+        </div>
 
-      <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-6 sm:px-10">
-          <Form className="mb-0 space-y-6" method="POST"
-                onSubmit={(event) => {
-                  handleSubmit(() => submit(event.target))(event)
-                }}>
+        <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-6 sm:px-10">
+            <Form className="mb-0 space-y-6" method="POST"
+                  onSubmit={(event) => {
+                    handleSubmit(() => submit(event.target))(event)
+                  }}>
               <FieldInput type="email" label="Email" name="email" register={register} errors={errors} />
 
               <FieldInput type="password" label="Password" name="password" register={register} errors={errors} />
 
-            <div>
-              <button
-                      disabled={isSubmitting}
-                      className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white
+              <div>
+                <button
+                  disabled={isSubmitting}
+                  className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white
                 no-underline bg-black border border-transparent rounded-md bg-black
                 text-white hover:bg-blue-600
                 md:py-3 md:text-lg md:px-10 md:leading-6">
-                {
-                  isSubmitting ? "Processing..." : "Login"
-                }
-              </button>
-            </div>
-          </Form>
+                  {
+                    isSubmitting ? "Processing..." : "Login"
+                  }
+                </button>
+              </div>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
